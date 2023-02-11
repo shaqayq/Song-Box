585dbbb61584d0f573021fe487221c767a9207e1
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-12 00:11:05 +0430

    fix eslin errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,18 @@ import '../style/navbar.css';
 const Navbar = () => (
   <>
     <nav className="navbar shadow-sm p-3 mb-5 bg-white rounded">
-    <div id='logo'>
-      <img src={mobile} className="logo" alt="LOGO" />
-      <h4 className="title">Mobile Zone</h4>
+      <div id="logo">
+        <img src={mobile} className="logo" alt="LOGO" />
+        <h4 className="title">Mobile Zone</h4>
       </div>
-      <div id='social'>
-      <i>
-        {' '}
-        <FaInstagram />
-        {' '}
-      </i>
-      <i><FaTwitter /></i>
-      <i><FaFacebook /></i>
+      <div id="social">
+        <i>
+          {' '}
+          <FaInstagram />
+          {' '}
+        </i>
+        <i><FaTwitter /></i>
+        <i><FaFacebook /></i>
       </div>
     </nav>
   </>
