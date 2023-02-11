a080342eab8de0cb7a25bcd5f1e38a1aefd04834
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-12 00:06:31 +0430

    add social icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => (
       <img src={mobile} className="logo" alt="LOGO" />
       <h4 className="title">Mobile Zone</h4>
       </div>
+      <div id='social'>
       <i>
         {' '}
         <FaInstagram />
@@ -17,7 +18,7 @@ const Navbar = () => (
       </i>
       <i><FaTwitter /></i>
       <i><FaFacebook /></i>
-
+      </div>
     </nav>
   </>
 );
